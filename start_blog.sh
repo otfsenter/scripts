226e6bb35826e13d528422dffb755b69e5608d34
otfsenter <zxzotfsent@gmail.com>
2018-10-24 11:48:18 -0400

    add start_blog.sh

---
+++ start_blog.sh
@@ -0,0 +1,108 @@
+#!/usr/bin/bash
+
+yum -y update
+yum -y install yum-utils
+yum -y groupinstall development
+
+
+# install python
+yum -y install https://centos7.iuscommunity.org/ius-release.rpm
+yum -y install python36u
+yum -y install python36u-pip
+yum -y install python36u-devel
+ln -s /usr/bin/python3.6 /usr/bin/python3
+ln -s /usr/bin/pip3.6 /usr/bin/pip3
+pip3 install django
+pip3 install uwsgi
+
+# install nginx 
+cat << EOF > /etc/yum.repos.d/nginx.repo
+[nginx]
+name=nginx repo
+baseurl=http://nginx.org/packages/centos/7/$basearch/
+gpgcheck=0
+enabled=1
+EOF
+
+yum install -y nginx
+
+mv /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/default.conf_backup
+cat << EOF > /etc/nginx/conf.d/mysite.conf
+
+# the upstream component nginx needs to connect to
+upstream django {
+    # server unix:///path/to/your/mysite/mysite.sock; # for a file socket
+    server 127.0.0.1:8001; # for a web port socket (we'll use this first)
+}
+
+# configuration of the server
+server {
+    # the port your site will be served on
+    listen      80;
+    # the domain name it will serve for
+    server_name server_name; # substitute your machine's IP address or FQDN
+    access_log  /var/log/nginx/access.log;
+    error_log   /var/log/nginx/error.log;
+    charset     utf-8;
+    gzip on;
+    gzip_types text/plain application/x-javascript text/css text/javascript application/x-httpd-php application/json text/json image/jpeg image/gif image/png application/octet-stream;
+
+    error_page  404           /404.html; 
+    error_page   500 502 503 504  /50x.html;
+
+    # max upload size
+    client_max_body_size 75M;   # adjust to taste
+
+    # Django media
+    location /media/ {
+        alias /root/code/django_blog/media/;  # your Django project's media files - amend as required
+    }
+
+    location /static/ {
+        alias /root/code/django_blog/static/;
+    }
+
+    # Finally, send all non-media requests to the Django server.
+    location / {
+        include uwsgi_params;
+        uwsgi_connect_timeout 30;
+        uwsgi_pass unix:/root/code/django_blog/uwsgi.sock;
+    }
+}
+EOF
+
+
+# download django blog 
+mkdir -p /root/code/
+cd /root/code/
+git clone https://github.com/otfsenter/django_blog.git
+touch  uwsgi.log uwsgi.sock uwsgi.ini uwsgi.pid
+
+cat << EOF > uwsgi.ini
+[uwsgi]
+chdir=/root/code/django_blog
+module=django_blog.wsgi:application
+workers=5
+pidfile=/root/code/django_blog/uwsgi.pid
+http=127.0.0.1:8001
+static-map=/static=/root/code/django_blog/static
+uid=root
+gid=root
+master=true
+vacuum=true
+thunder-lock=true
+enable-threads=true
+harakiri=30
+post-buffering=4096
+daemonize=/root/code/django_blog/uwsgi.log
+socket=/root/code/django_blog/uwsgi.sock
+EOF
+
+
+echo "collectstatic"
+echo "nginx server name"
+echo "then start uwsgi and nginx"
+
+
+
+
