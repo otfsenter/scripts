b342cdc4c56a85876cff5190a09681e806d6719e
otfsenter <newellzhou@163.com>
2020-12-05 00:49:33 +0800

    update readme for scripts

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# Python execrise scripts test 2
+# Python execrise scripts
+
+## auto move files
+
+自动移动归档文件当指定的文件夹
+
+auto mv files to special directory
+
+## idle
+
+nginx.sh 
+
+* rm idle container exist
+* start nginx docker container 
+
+## blog
+
+* .nojekyll
+
+指定 index.html 为首页
+
+* favicon.ico
+
+网站标签左边的图片
+
+## java
+
+* settings.xml
+
+aliyun mirros configuration of maven
+
+
+## aliyun.conf
+
+the configuration of nginx in aliyun server with https
+
+## delimiters.py
+
+自定义实现的多个分隔符分割一段字符串
+
+## docker-compose.yml
+
+启动 wordpress 容器的配置，包含 mysql
+
+## flow_real_time.py
+
+* python版本：python3
+* 第三方库：psutil
+* 是什么：显示实时的流量，用 tkinter 做了一个图形界面，
+* 怎么运行：点击运行脚本之后，点击 start
+* 频率：一秒刷新一次
--- delimiters.py
+++ delimiters.py
@@ -1,10 +1,4 @@
-#!/usr/bin/env python
-# _*_ coding: utf-8 _*_
-# @Time     : 2017/3/9 19:52
-# @Author   : otfsenter
-# @File     : a.py
-
-#coding:utf-8
+from functools import reduce
 
 result = '''
 sdf-asd
@@ -14,12 +8,6 @@ sdf-asd,sdf-asd01 ,sdf-asd02
 aui+otfsenter+which
 '''
 
-# result = ''
-# with open('tmp.txt', 'r') as f:
-#     for i in f:
-#         result += i
-#
-# print result
 
 def split_by_separator(string='', separators=','):
     rst = [string]
@@ -32,4 +20,5 @@ def split_by_separator(string='', separators=','):
     [list_tmp.append(data) for data in rst if data != '']
     return reduce(lambda x, y: y in x and x or x + [y], [[], ] + list_tmp)
 
-print split_by_separator(result, '\n,+')
+
+print(split_by_separator(result, '\n,+'))
